38ae3ac7635ed951c30007f7c3c37ef36525e7ff
torexxx <molento-studio@yandex.ru>
2021-01-24 00:14:02 +0700

    initial commit

---
+++ precache-manifest.506453f89444ec2d27f8b3fd25dc6d63.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc847b92996a515963bdd76b6f7d472",
+    "url": "/react-soc-network-demo/index.html"
+  },
+  {
+    "revision": "ada9dc64b23dc65a4cc7",
+    "url": "/react-soc-network-demo/static/css/3.c0f17746.chunk.css"
+  },
+  {
+    "revision": "1f00fb4272c14d363a2b",
+    "url": "/react-soc-network-demo/static/css/4.4419c4ef.chunk.css"
+  },
+  {
+    "revision": "f2809c905759ea79b44f",
+    "url": "/react-soc-network-demo/static/css/main.c25d124e.chunk.css"
+  },
+  {
+    "revision": "9501a83b5fde7fc02ed6",
+    "url": "/react-soc-network-demo/static/js/2.b1d48d8e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-soc-network-demo/static/js/2.b1d48d8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada9dc64b23dc65a4cc7",
+    "url": "/react-soc-network-demo/static/js/3.d4ed59b5.chunk.js"
+  },
+  {
+    "revision": "1f00fb4272c14d363a2b",
+    "url": "/react-soc-network-demo/static/js/4.80c0a153.chunk.js"
+  },
+  {
+    "revision": "f2809c905759ea79b44f",
+    "url": "/react-soc-network-demo/static/js/main.246004f5.chunk.js"
+  },
+  {
+    "revision": "68e0fb420f306572c160",
+    "url": "/react-soc-network-demo/static/js/runtime-main.a8ece8f3.js"
+  },
+  {
+    "revision": "029ca91fe21157b8cfccd348ba7b000b",
+    "url": "/react-soc-network-demo/static/media/avatar.029ca91f.png"
+  },
+  {
+    "revision": "4721fd35c8647513016a9516d677dde6",
+    "url": "/react-soc-network-demo/static/media/logo.4721fd35.png"
+  }
+]);
